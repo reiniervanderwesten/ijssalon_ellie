05a8d13a57f222b06db00c2ae3027809b8b248b1
naam <reiniervanderwesten@hotmail.com>
2023-07-31 19:25:36 +0200

    toch maar aardbei voor goede uitkomst

--- tijdelijk.py
+++ tijdelijk.py
@@ -3,8 +3,12 @@ prijzen= {"aardbei": 3,
           "chocolade": 5
          }
 
-aanbieding= 0.8* prijzen["vanille"]
-
-
-reclame_tekst= f"vandaag in de aanbieding: vanille-ijs, 1 liter- slechts € {aanbieding}"
+aanbieding= 0.8* prijzen["aardbei"]
+'''
+Aan de docent: in de huiswerkopdracht staat dat ik vanille moet nemen voor de aanbieding. Als ik dit doe krijg ik 3.2 als uitkomst.
+Volgens het plaatje bij de huiswerkopdracht moet ik echter 2.4 met veel nullen achter de komma krijgen. 
+Dit is wel het geval als ik aardbei kies voor de variabele aanbieding.
+Met het oog op het vervolg van de opdracht ga ik daarom uit van aardbei voor de variabele aanbieding. 
+'''
+reclame_tekst= f"vandaag in de aanbieding: aardbei-ijs, 1 liter- slechts € {aanbieding}"
 print(reclame_tekst)
