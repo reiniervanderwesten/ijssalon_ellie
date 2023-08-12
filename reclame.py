58047403f3c2199384fac3807eacc280fc89be70
naam <reiniervanderwesten@hotmail.com>
2023-08-12 16:30:29 +0200

    aanbieding_1()

---
+++ reclame.py
@@ -0,0 +1,7 @@
+def aanbieding_1(smaak, prijs, korting):
+    
+    print(f"vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {((1-korting)* prijs)} euro.")
+    
+
+
+aanbieding_1("aardbei", 4, 0.1)
