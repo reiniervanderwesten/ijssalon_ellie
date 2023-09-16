93d78cd5be2430a27b41077bacba116a363db08d
naam <reiniervanderwesten@hotmail.com>
2023-09-16 12:13:50 +0200

    opgave 10

--- boekhouding.py
+++ boekhouding.py
@@ -4,4 +4,6 @@ from helper import *
 inkomsten={"Aardbeienijs-ijs-totaal": 1000, "Vanille-ijs-totaal": 2000, "Chocolade-ijs-totaal": 1500, "Waterijsjes-totaal": 750}
 
 totaal_inkomsten=som(inkomsten)
-print(totaal_inkomsten)
+
+
+presenteer(inkomsten, totaal_inkomsten)
