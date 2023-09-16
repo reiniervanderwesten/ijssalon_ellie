361e615d0bafa96a3c1644d9ef57a359216f32af
naam <reiniervanderwesten@hotmail.com>
2023-09-16 12:40:21 +0200

    opgave 11

--- boekhouding.py
+++ boekhouding.py
@@ -1,6 +1,7 @@
 
 from presentatie import *
 from helper import *
+import csv
 inkomsten={"Aardbeienijs-ijs-totaal": 1000, "Vanille-ijs-totaal": 2000, "Chocolade-ijs-totaal": 1500, "Waterijsjes-totaal": 750}
 
 totaal_inkomsten=som(inkomsten)
